05838ac080b455a20bca1ced78c0cfa978735820
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-10-22 14:37:57 +0100

    Update UniversiteServiceite

---
+++ src/main/java/esprit/tn/amdounidev/Repository/UniversiteRepository.java
@@ -0,0 +1,7 @@
+package esprit.tn.amdounidev.Repository;
+
+import esprit.tn.amdounidev.entities.Universite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UniversiteRepository extends JpaRepository<Universite,Long> {
+}
