2b9d2470e563f1bd4a0cd678a80239163f236b62
Mohamed Aziz Ben Ismail <aziz.270700@gmail.com>
2022-10-27 16:37:30 +0100

    Update UniversiteRepository.java

--- src/main/java/esprit/tn/amdounidev/Repository/UniversiteRepository.java
+++ src/main/java/esprit/tn/amdounidev/Repository/UniversiteRepository.java
@@ -3,5 +3,19 @@ package esprit.tn.amdounidev.Repository;
 import esprit.tn.amdounidev.entities.Universite;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface UniversiteRepository extends JpaRepository<Universite,Long> {
+
+    //select * from Universite where nomUniversite=?
+    List<Universite> findBynomUniversite(String nomUniversite);
+
+/*
+    List<Universite> getBydateFinC(Date dateFinC);
+    //select * from Contrat where nom=? and prenom
+//zdvccccccccccc
+    List<Universite> findByspecialite(String specialite);
+    List<Universite> findByMontantCBetween(float min,float Max);
+ */
 }
